af5d4905963544d44f21700379586e47f16ac759
CALIQUIS Cristian Angel <cristian-angel.caliquis@danone.com>
2024-10-30 12:55:55 -0300

    punto precio max hecho

--- 1.c
+++ 1.c
@@ -11,13 +11,18 @@ e. Listar los artículos por precio en orden ascendente. (2 puntos)*/
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int productos_precio[5][2];
+float precios[5];
+int prod = 0;
+int n = 0;
+float precio_max = 0.0;
+int codigo_producto_max = 0;
+
+void precio_maximo(float precios[], int n, int *indice);
+
 int main(int argc, char *argv[])
 {
-
-    int productos_precio[5][2];
-    float precios[5];
-    int prod = 0;
-
     for (prod = 0; prod < 5; prod++)
     {
         productos_precio[prod][0] = 0;
@@ -28,6 +33,7 @@ int main(int argc, char *argv[])
     {
         printf("Ingrese el codigo del producto y el precio: %d  ", prod + 1);
         scanf("%d %f", &productos_precio[prod][0], &precios[prod]);
+        
     }
 
     printf("\nProductos y precios ingresados:\n");
@@ -36,5 +42,33 @@ int main(int argc, char *argv[])
         printf("Producto: %d, Codigo: %d,  Precio: %.2f\n", prod + 1, productos_precio[prod][0], precios[prod]);
     }
 
+
+    int indice_max = 0;  
+    precio_maximo(precios, 5, &indice_max);
+    codigo_producto_max = productos_precio[indice_max][0];
+    precio_max = precios[indice_max];
+    //*indice al ser un puntero, tambien se actualiza en la funcion main, con el indice de precio maximo.
+
+    printf("\nEl precio máximo es: %.2f, correspondiente al producto con código: %d\n", precio_max, codigo_producto_max);
+
     return 0;
+
+
+    return 0;
+}
+
+void precio_maximo(float precios[], int n, int *indice)
+{
+    float max = precios[0];       //se inicializa el primer elemento, suponiendo que el primero es el maximo
+    *indice = 0;                  //tambien se inicializa, suponiendo que el primer producto tiene el precio max en la posicion 0
+    for (int i = 1; i < n; i++)
+    {
+        if (precios[i] > max)
+        {
+            max = precios[i];
+            *indice = i;
+        }                         //este bucle recorre el vector y va iterando. Si precios[i] es > que max, max se va
+                                  //actualizando para que sea = a precio[i]. (el nuevo maximo)
+                                  //y *indice se actualiza a i y almacena en donde se encuentra este nuevo precio maximo.
+    }
 }
